4058449fb473752a2e813cd00e7470ed61cf3aeb
Kursat <35656872+kozdalli@users.noreply.github.com>
2020-05-17 16:50:49 +0300

    Update __init__.py

--- odev3/__init__.py
+++ odev3/__init__.py
@@ -11,7 +11,7 @@ def compiles():
     """odev3.c compiles"""
     check50.c.compile("odev3.c")
     
-    @check50.check(compiles)
-def test0():
-    """0 cikis sagliyor mu"""
-    check50.run("./odev3").stdin("0").stdout("PROGRAM SONLANMISTIR\n").stdout(check50.EOF).exit(0)
+@check50.check(compiles)
+def test2():
+    """identifies 371449635398431 as AMEX"""
+    check50.run("./odev3").stdin("371449635398431").stdout("AMEX\n").stdout(check50.EOF).exit(0)
