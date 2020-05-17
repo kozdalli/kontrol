224b9c728528d29944b792ca677f11accf0c955f
Kursat <35656872+kozdalli@users.noreply.github.com>
2020-05-17 16:54:01 +0300

    Update __init__.py

--- odev3/__init__.py
+++ odev3/__init__.py
@@ -12,57 +12,56 @@ def compiles():
     check50.c.compile("odev3.c")
     
 @check50.check(compiles)
-@check50.check(compiles)
 def test1():
-    """identifies 378282246310005 as AMEX"""
+    """378282246310005 AMEX"""
     check50.run("./odev3").stdin("378282246310005").stdout("AMEX\n").stdout(check50.EOF).exit(0)
 
 @check50.check(compiles)
 def test2():
-    """identifies 371449635398431 as AMEX"""
+    """371449635398431 AMEX"""
     check50.run("./odev3").stdin("371449635398431").stdout("AMEX\n").stdout(check50.EOF).exit(0)
 
 @check50.check(compiles)
 def test3():
-    """identifies 5555555555554444 as MASTERCARD"""
+    """5555555555554444 MASTERCARD"""
     check50.run("./odev3").stdin("5555555555554444").stdout("MASTERCARD\n").stdout(check50.EOF).exit(0)
 
 @check50.check(compiles)
 def test4():
-    """identifies 5105105105105100 as MASTERCARD"""
+    """5105105105105100 MASTERCARD"""
     check50.run("./odev3").stdin("5105105105105100").stdout("MASTERCARD\n").stdout(check50.EOF).exit(0)
 
 @check50.check(compiles)
 def test5():
-    """identifies 4111111111111111 as VISA"""
+    """4111111111111111 VISA"""
     check50.run("./odev3").stdin("4111111111111111").stdout("VISA\n").stdout(check50.EOF).exit(0)
 
 @check50.check(compiles)
 def test6():
-    """identifies 4012888888881881 as VISA"""
+    """4012888888881881 VISA"""
     check50.run("./odev3").stdin("4012888888881881").stdout("VISA\n").stdout(check50.EOF).exit(0)
 
 @check50.check(compiles)
 def test7():
-    """identifies 1234567890 as GECERSIZ"""
+    """1234567890 GECERSIZ"""
     check50.run("./odev3").stdin("1234567890").stdout("GECERSIZ\n").stdout(check50.EOF).exit(0)
 
 @check50.check(compiles)
 def test8():
-    """identifies 369421438430814 as INVALID"""
+    """369421438430814 GECERSIZ"""
     check50.run("./odev3").stdin("369421438430814").stdout("GECERSIZ\n").stdout(check50.EOF).exit(0)
 
 @check50.check(compiles)
 def test9():
-    """identifies 4062901840 as INVALID"""
+    """4062901840 GECERSIZ"""
     check50.run("./odev3").stdin("4062901840").stdout("GECERSIZ\n").stdout(check50.EOF).exit(0)
 
 @check50.check(compiles)
 def test10():
-    """identifies 5673598276138003 as INVALID"""
+    """5673598276138003 GECERSIZ"""
     check50.run("./odev3").stdin("5673598276138003").stdout("GECERSIZ\n").stdout(check50.EOF).exit(0)
 
 @check50.check(compiles)
 def test11():
-    """identifies 4111111111111113 as INVALID"""
-    check50.run("./odev3").stdin("4111111111111113").stdout("INVALID\n").stdout(check50.EOF).exit(0)
+    """4111111111111113 GECERSIZ"""
+    check50.run("./odev3").stdin("4111111111111113").stdout("GECERSIZ\n").stdout(check50.EOF).exit(0)
