3dbfcb4c2293f00c30820224707f05ed93455b74
Kursat <35656872+kozdalli@users.noreply.github.com>
2020-05-17 17:15:14 +0300

    Update __init__.py

--- odev3/__init__.py
+++ odev3/__init__.py
@@ -69,6 +69,4 @@ def test11():
 @check50.check(compiles)
 def test00():
     """-5 -> Kart No: """
-    check50.run("./odev3").stdin("-5").stdout("Kart No: ").stdin("-10").stdout("Kart No: ")
-    
-
+    check50.run("./odev3").stdin("-5").stdout("Kart No: ").stdin("9").stdout("GECERSIZ\n").stdout(check50.EOF).exit(0)
